b15bd6e92d39c888e46e7a0c499e8d11dad94cbe
Riccardo Persello <riccardo.persello+i@icloud.com>
2022-10-28 11:34:42 +0200

    Non-volatile CCCD implementation.

--- src/gatt_server/custom_attributes.rs
+++ src/gatt_server/custom_attributes.rs
@@ -1,7 +1,20 @@
+use std::sync::{Arc, Mutex};
+
 use crate::{
     gatt_server::Descriptor,
     utilities::{AttributePermissions, BleUuid},
 };
+
+use embedded_svc::storage::RawStorage;
+use esp_idf_svc::nvs::EspDefaultNvs;
+use esp_idf_svc::nvs_storage::EspNvsStorage;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref STORAGE: Mutex<EspNvsStorage> = Mutex::new(
+        EspNvsStorage::new_default(Arc::new(EspDefaultNvs::new().unwrap()), "ble", true).unwrap()
+    );
+}
 
 impl Descriptor {
     pub fn user_description<S: AsRef<str>>(description: S) -> Self {
@@ -20,7 +33,35 @@ impl Descriptor {
             BleUuid::from_uuid16(0x2902),
             AttributePermissions::read_write(),
         )
-        .set_value(0u16.to_le_bytes())
+        .on_read(|param| {
+            let storage = STORAGE.lock().unwrap();
+
+            // Create a key from the connection address.
+            let key = format!(
+                "{:02X}:{:02X}:{:02X}:{:02X}:{:02X}:{:02X}",
+                param.bda[0], param.bda[1], param.bda[2], param.bda[3], param.bda[4], param.bda[5]
+            );
+
+            // Prepare buffer and read correct CCCD value from non-volatile storage.
+            let mut buf: [u8; 2] = [0; 2];
+            if let Some(value) = storage.get_raw(&key, &mut buf).unwrap() {
+                value.0.to_vec()
+            } else {
+                vec![0, 0]
+            }
+        })
+        .on_write(|value, param| {
+            let mut storage = STORAGE.lock().unwrap();
+
+            // Create a key from the connection address.
+            let key = format!(
+                "{:02X}:{:02X}:{:02X}:{:02X}:{:02X}:{:02X}",
+                param.bda[0], param.bda[1], param.bda[2], param.bda[3], param.bda[4], param.bda[5]
+            );
+
+            // Write CCCD value to non-volatile storage.
+            storage.put_raw(&key, &value).unwrap();
+        })
         .to_owned()
     }
 }
