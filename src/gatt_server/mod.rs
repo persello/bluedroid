d57d29eb8f54d484a223026a8827f1895ab73601
Riccardo Persello <riccardo.persello@icloud.com>
2023-03-27 21:37:05 +0200

    Changed some #cfg[]s.

--- src/gatt_server/mod.rs
+++ src/gatt_server/mod.rs
@@ -233,6 +233,7 @@ impl GattServer {
             pcm_polar: CONFIG_BTDM_CTRL_PCM_POLAR_EFF as _,
             hli: BTDM_CTRL_HLI != 0,
             magic: ESP_BT_CONTROLLER_CONFIG_MAGIC_VAL,
+            dup_list_refresh_period: DUPL_SCAN_CACHE_REFRESH_PERIOD as u16,
         };
 
         #[cfg(esp32c3)]
@@ -257,20 +258,22 @@ impl GattServer {
             txant_dft: CONFIG_BT_CTRL_TX_ANTENNA_INDEX_EFF as u8,
             rxant_dft: CONFIG_BT_CTRL_RX_ANTENNA_INDEX_EFF as u8,
             txpwr_dft: CONFIG_BT_CTRL_DFT_TX_POWER_LEVEL_EFF as u8,
-            cfg_mask: CFG_MASK,
+            cfg_mask: CFG_NASK,
             scan_duplicate_mode: SCAN_DUPLICATE_MODE as u8,
             scan_duplicate_type: SCAN_DUPLICATE_TYPE_VALUE as u8,
             normal_adv_size: NORMAL_SCAN_DUPLICATE_CACHE_SIZE as u16,
             mesh_adv_size: MESH_DUPLICATE_SCAN_CACHE_SIZE as u16,
             coex_phy_coded_tx_rx_time_limit: CONFIG_BT_CTRL_COEX_PHY_CODED_TX_RX_TLIM_EFF as u8,
-            #[cfg(esp_idf_version = "5.0")]
+            #[cfg(any(esp_idf_version = "4.4", esp_idf_version = "5.0"))]
             hw_target_code: BLE_HW_TARGET_CODE_ESP32C3_CHIP_ECO0,
             #[cfg(esp_idf_version = "5.1")]
             hw_target_code: BLE_HW_TARGET_CODE_CHIP_ECO0,
             slave_ce_len_min: SLAVE_CE_LEN_MIN_DEFAULT as u8,
             hw_recorrect_en: AGC_RECORRECT_EN as u8,
             cca_thresh: CONFIG_BT_CTRL_HW_CCA_VAL as u8,
+            // #[cfg(any(esp_idf_version = "5.0", esp_idf_version = "5.1"))]
             scan_backoff_upperlimitmax: BT_CTRL_SCAN_BACKOFF_UPPERLIMITMAX as u16,
+            // #[cfg(any(esp_idf_version = "5.0", esp_idf_version = "5.1"))]
             dup_list_refresh_period: DUPL_SCAN_CACHE_REFRESH_PERIOD as u16,
             #[cfg(esp_idf_version = "5.1")]
             ble_50_feat_supp: BT_CTRL_50_FEATURE_SUPPORT != 0,
@@ -304,7 +307,7 @@ impl GattServer {
             normal_adv_size: NORMAL_SCAN_DUPLICATE_CACHE_SIZE as u16,
             mesh_adv_size: MESH_DUPLICATE_SCAN_CACHE_SIZE as u16,
             coex_phy_coded_tx_rx_time_limit: CONFIG_BT_CTRL_COEX_PHY_CODED_TX_RX_TLIM_EFF as u8,
-            #[cfg(esp_idf_version = "5.0")]
+            #[cfg(any(esp_idf_version = "4.4", esp_idf_version = "5.0"))]
             hw_target_code: BLE_HW_TARGET_CODE_ESP32S3_CHIP_ECO0,
             #[cfg(esp_idf_version = "5.1")]
             hw_target_code: BLE_HW_TARGET_CODE_CHIP_ECO0,
